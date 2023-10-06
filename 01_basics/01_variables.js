e131346a6d8e6482ab0883d478ba5557dd942e06
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-06 16:25:51 +0000

    ver2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "sujal@gmail.com"
+// var accountPassword = " 123456"
+
+
+accountCity = " Jaipur"
+
+// accountId = 3323232
+accountEmail = "lochan@homail.co.uk"
+accountPassword = " 9812300033234234234324324234324"
+accountCity = " Hile "
+
+let accountState;
+
+accountCity = "Kathmandu"
+accountCity = "Samsung"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+/*
+prefer not use Var 
+because of issue in block scope and functional scope//
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+// "use strict"; // treat all JS code as Newer version 
+
+//alert(4+4) we are using nodejs, not browser 
+
+console.log(3+3+3+4+4+23423423423423432+234234*324234234324323) 
+
+console.log("Dictionary")

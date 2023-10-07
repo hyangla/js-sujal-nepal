edc3f4ffde21c65633b2d177968173028dc42f1d
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-07 15:36:35 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,6 +2,28 @@
 
 //alert(4+4) we are using nodejs, not browser 
 
-console.log(3+3+3+4+4+23423423423423432+234234*324234234324323) 
-
+console.log(3+3+3+4+4+23423423423423432+
+    234234*324234234324323) 
+// cdoe readability should be in order / or hight 
 console.log("Dictionary")
+
+
+let name = " Sujal "
+let age = 19 
+let isLogIn = false 
+let state = null
+
+
+// number  => to power 53 
+// bigint
+// string => "...."
+// boolean => true/false 
+// null => standalone value 
+// undefine => not value define 
+// symbol => define as uniqueness status 
+
+//object
+
+console.log(typeof null);
+console.log(typeof object);
+

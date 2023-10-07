3ad595842bb22f9add9dcdd1f03d9ffd8482831f
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-07 16:16:01 +0000

    conversionOfDataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = "sujal"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof 30);
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN not a number 
+
+
+// " 33" => 33 easy convery 
+// "33abc" => NaN ( Not a number)
+// True => 1 (Bolen); false => 0 ( Bolen)
+// "sujal" => string 
+// null = > 0 
+// undefine = Nan 
+
+
+
+let isloggedIn = "1"
+let boolenIsLoggedIn = Boolean(isloggedIn)
+
+// console.log(boolenIsLoggedIn);
+
+
+// 1 => True ; 0 => False 
+// "" => False 
+// "Sujal " => True 
+
+
+
+
+let NumberS= 33 
+
+let stringNumber = String(NumberS)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);

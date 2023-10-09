3f56f79892f46acf28fb08fa58d7c798f9df6693
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-09 07:57:18 +0000

    dataTypeSummary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,45 @@
+// Primitive 
+
+// 7 types : String, Boolen , Number , null, undefined, Symbol, 
+// BigInt
+
+//example are as follow as reminder :
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+
+let userEmail;
+
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNuber = 34234234234234234n
+
+
+
+
+// Reference (Non primitives )
+
+
+
+
+// * To Master JavaScript you must learn Objects and web events 
+
+// Array, Objects , Functions  //
+
+const hero = ["Shaktiman", "naagraj", "doga"];
+let myObj = {
+        fullName : "Sujal",
+        myAge:  35  ,
+        myAddress:"Dhankuta -1, Hile",
+}   
+
+console.log(hero);
+
+const = function()

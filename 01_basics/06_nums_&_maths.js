bb4e247235b404061945b0d0643964485e07de12
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-10 17:00:03 +0000

    Math and num

---
+++ 01_basics/06_nums_&_maths.js
@@ -0,0 +1,48 @@
+
+// const score = 400 
+// const balance = new Number(100000000023432423)
+
+
+// console.log(score);
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(typeof balance);
+
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 1222.8955 
+
+// console.log(otherNumber.toPrecision(4));
+
+
+// console.log(balance.toLocaleString('en-IN'));
+
+
+
+//++++++++++++++ Maths +++++++++++++++++++/
+
+// console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+
+
+
+console.log(Math.max(2,34,3,4343,+3000000000434343,23,12,1,2,33,4,4,5,))
+console.log(Math.min(-1, 0, 2,3,4,5,6,5));
+
+
+console.log(Math.random());
+console.log((Math.random()* 2) + 1);
+
+
+const min = 10
+const max = 20 
+
+console.log(Math.ceil(Math.random() * ( max - min + 1 )) + min )
+

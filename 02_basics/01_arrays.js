8e675e1f763b1b029c6a4fbe322d3773e26ee6a2
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-11 09:06:01 +0000

    arraysJs

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,12 @@
+// Array 
+
+const myArray = [ 1, 2, 3 ,4 ,5,6 ]
+
+myArray.push(10)
+myArray.push(11)
+// myArray.pop()
+// myArray.shift()
+console.log(myArray);
+
+console.log(myArray.includes(2))
+console.log(myArray.indexOf(2));

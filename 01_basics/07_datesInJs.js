c0a34e4b495457e99dda4448369b8f5e3008beb4
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-11 04:02:38 +0000

    dateInJs

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,24 @@
+// Dates 
+
+let myDate = new Date()
+console.log(myDate.toLocaleString());
+console.log();
+
+
+
+// let myCreateDate = new Date (2023,0,1)
+let myCreateDate = new Date ('2023-01-1')
+
+
+
+console.log(myCreateDate.toLocaleString());
+
+//  ++++++ Console Output ++++++
+
+// .toString = Wed Oct 11 2023 03:29:42 GMT+0000 (Coordinated Universal Time)
+// .toDateString = Wed Oct 11 2023
+// .toISOString = 2023-10-11T03:30:52.941Z
+// .toJSON = 2023-10-11T03:36:19.022Z
+// .toLocalDateString = 10/11/2023
+// .toLocalString = 10/11/2023, 3:37:51 AM
+

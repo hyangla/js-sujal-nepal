a537a0b76fa5d2b853036feae37a28f89a056cbf
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-14 08:32:58 +0000

    funcations

---
+++ 03_basics/01_funcations.js
@@ -0,0 +1,98 @@
+
+
+
+// function addingMyName(){
+    
+//     console.log("Dr.");
+//     console.log("Sabina");
+//     console.log("Tamang");
+// }
+
+// // addingMyName()
+
+// function zalTMG(){
+//     console.log("S");
+//     console.log("U");
+//     console.log("J");
+//     console.log("A");
+//     console.log("L");
+// }
+
+// // zalTMG()
+
+
+
+// function addTwoNumber(Numer1, Number2){             // thoses inside (defination) prenthasis is call                                                         parameter // 
+
+//   console.log(Numer1 + Number2);  
+
+// }
+
+// addTwoNumber(100,)                                    // this call arguments  
+
+
+
+// function addTwoNumber(Numer1, Number2){             
+
+//     let result = Numer1 + Number2
+//     return result
+// }
+
+
+// const result = addTwoNumber(4,3)
+
+// console.log("result:", result);
+
+
+
+
+// function myNameIs (fname,lname){
+//     console.log(fname + lname);
+// }
+
+
+
+// function myNameIs (fname,lname){
+//     // let result = fname + lname
+//     // return result
+
+//     return fname+lname
+// }
+
+// const result = myNameIs(3,3)
+
+// // console.log ( "the Resulat Value is : ",result );
+
+// function logInMessage (username ){
+//     if (!username) {               //PTBN  // we dont need to use === to undefine object / "!" will gonna sort it out . 
+//         console.log("Please Enter Uername");
+//         return 
+
+//     }
+//     return `${username} is just logged in` 
+// }
+
+// // console.log((logInMessage("Sujal")));
+// console.log((logInMessage()));
+
+
+// function calculateCartPrice(val1,val2,...nu1){
+// return nu1
+
+// }
+
+// console.log(calculateCartPrice(200,400,4000,433,3443));
+
+
+const user = {
+    usernames: "Sujal Tamang ",
+    prices : 232
+
+}
+
+
+function handleobject (anyobject){
+console.log(`username is ${user.username} petrol  price is ${user.price}`);
+}
+
+handleobject( )

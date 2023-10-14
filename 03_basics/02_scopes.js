e3a03e052dc914285c8731ecb48db4d16ce732d2
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-14 08:57:55 +0000

    scopesAll

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+
+
+                                        //Global Scoop 
+let a = 300
+
+if (true) {
+        let a = 10 
+        const b = 20                      //Block Scoop 
+        // var c = 30    
+
+        console.log(a);
+}
+
+console.log(a);

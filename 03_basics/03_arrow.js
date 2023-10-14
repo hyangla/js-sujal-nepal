b85efa3a4b0b42a93cc6307bcff5cde1c130ec2a
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-14 14:50:40 +0000

    ArrowInfo

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,51 @@
+// const user = {
+//     username: "Sujal",
+//     price : 333,
+
+
+//     welcomeMessage :  function (){
+//         console.log(`${this.username}, welcome to website`);
+//         console.log(`${this.price}, is the price of petrol`);
+//         // console.log(this);
+    
+//     }
+    
+// }
+
+// user.welcomeMessage()
+// user.username = "animal"
+// user.price =  "100"
+
+// console.log(this);
+
+
+// function chaya (){
+//     const username = "Sujal"
+
+//     console.log(this.username);
+// }
+
+// chaya()
+
+
+// const chai = function(){
+//     const username = "ZAltmg"
+// console.log(this.username);
+// }
+// const chai = () =>{
+//     const username = "ZAltmg"
+// console.log(this);
+// }
+
+
+
+// const addTwo = (nu1, nun2) => {
+//     return nu1+nun2
+    
+//  }
+
+//  const addTwo = (nu1, nun2) => nu1+nun2
+//  const addTwo = (nu1, nun2) => {nu1 + nun2}
+
+// console.log(addTwo (3,2))
+

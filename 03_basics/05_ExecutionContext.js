007224a94da1950ac22fb7dcd78e558d32aa42e7
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-15 16:29:24 +0000

    Execution

---
+++ 03_basics/05_ExecutionContext.js
@@ -0,0 +1,10 @@
+// JavaScript Execution context 
+// [code] =>  Global EC 
+// ++++++++
+// > Global Execution Context 
+// > Function Execution Context 
+// > Eval Execution Context 
+
+// [CODE] =
+// > Memory Creation Phase 
+// >Execution Phase 

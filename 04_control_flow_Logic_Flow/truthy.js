fbdc3420d33423fc2d1bdb48b05eccb47a6afd05
hyangla <81016857+hyangla@users.noreply.github.com>
2023-10-16 16:30:41 +0000

    push

---
+++ 04_control_flow_Logic_Flow/truthy.js
@@ -0,0 +1,22 @@
+
+
+const userEmail = { }
+
+if (userEmail) {
+    console.log("Got the user email ! Cheers now good to go .....");
+    
+}else console.log("Sorry ! can't find email. Terminited ");
+
+
+// Falsy Values ++++ [ false, 0 , -0, BigInt 0n , "" , null , undefined, Nan ]++++//
+// Truthy Value ++++ [ "0", "False", " " , [],{}, funcation(){}, 
+ 
+
+// following line of code check
+
+if (userEmail.length === 0) {
+    console.log("Array is empy");
+}
+
+
+
---
+++ 05_iterations/one.js
@@ -0,0 +1,59 @@
+// // for 
+
+
+// for (let i = 1; i < 11; i++) {
+//     const element = i;
+//     if (element == 5) {
+        
+//        // console.log( "5 is the best number (*_*)");
+//     }
+//    // console.log(element);
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value : ${i}`);
+
+
+
+//     for (let j = 0; j <= 10; j++) {
+//     //    console.log(`Inner loop value: ${j} and inner loop ${i}`);
+//        console.log(i + '*' + j + '=' + i * j);
+
+//     }
+    
+// }
+// }
+
+
+// let myArray = ["Sujal", "Ram", "Pk"]
+
+// for (let index = 0; index < myArrayarray.length; index++) {
+//     const element = myArrayarray[index];
+    
+//     console.log(element);
+// }
+
+
+    
+// }
+
+
+//++++++++++++++++++++Break and continue 
+
+// for (let index = 0; index <=20; index++) {
+//     if (index == 5) {
+//         console.log(`Dectected 5 `);
+//         break
+
+//     }
+//     console.log(`The value of Sujal is ${index}`);
+    
+// }
+
+for (let index = 0; index <=20; index++) {
+    if (index == 3) {
+        console.log(`Dectected 5 `);
+        continue
+
+    }
+    console.log(`The value of Sujal is ${index}`);
+    
+}
